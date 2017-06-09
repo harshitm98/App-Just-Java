8d1b1a156bee12ab61e446ed3132ed9764b2b88f
harshitm98 <harshitmah98@gmail.com>
2017-06-10 00:44:02 +0530

    Using variables, duh! :/

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2);
-        displayPrice(2*10);
+        int numberOfCoffees = 2;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees*10);
     }
 
     /**
